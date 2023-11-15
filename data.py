0c0124442c95bedbe5d36ba350625d1c6bb6edf7
gia-m12 <tul55523@temple.edu>
2023-11-15 18:14:15 -0500

    getting most popular nearby airports for users current location and desired destination. also getting airline codes for 5 popular airlines.

--- data.py
+++ data.py
@@ -1,29 +1,79 @@
 import requests
 import json
 
-params = {
+#first get airports nearby your location
+origin_params = {
     'api_key': '4c934e67-5e04-4d19-953e-eac352d72f50',
     'lat': '40.099941',
     'lng': '-74.97579',
     'distance': '50'
-    
 }
-url = 'https://airlabs.co/api/v9/nearby'
-response = requests.get(url, params=params)
-
-if response.status_code == 200:
-    data = response.json()
-    airports = data.get('response', {}).get('airports', [])
+origin_url = 'https://airlabs.co/api/v9/nearby'
+origin_response = requests.get(origin_url, params=origin_params)
+origin_airport = []
+if origin_response.status_code == 200:
+    origin_data = origin_response.json()
+    airports = origin_data.get('response', {}).get('airports', [])
+    origin_airport = airports[0]
     for airport in airports:
         if(airport.get("iata_code") != None):
-            print("Name:", airport.get("name"))
-            print("IATA Code:", airport.get("iata_code"))
-            print("ICAO Code:", airport.get("icao_code"))
-            print("Latitude:", airport.get("lat"))
-            print("Longitude:", airport.get("lng"))
-            print("Country Code:", airport.get("country_code"))
-            print("Popularity:", airport.get("popularity"))
-            print("Distance:", airport.get("distance"))
-            print()
+            if origin_airport.get("popularity") < airport.get("popularity"):
+                origin_airport = airport
 else:
-    print("API request failed with status code:", response.status_code)
+    print("API request failed with status code:", origin_response.status_code)
+#get destination airports
+des_params = {
+    'api_key': '4c934e67-5e04-4d19-953e-eac352d72f50',
+    'lat': '28.476224089314087',
+    'lng': '-81.46854344694752',
+    'distance': '50'
+}
+des_url = 'https://airlabs.co/api/v9/nearby'
+des_response = requests.get(des_url, params=des_params)
+
+if des_response.status_code == 200:
+    des_airports = []
+    des_data = des_response.json()
+    airports = des_data.get('response', {}).get('airports', [])
+    des_airport = airports[0]
+    for airport in airports:
+        if(airport.get("iata_code") != None):
+            if des_airport.get("popularity") < airport.get("popularity"):
+                des_airport = airport
+else:
+    print("API request failed with status code:", des_response.status_code)
+
+
+airline_params = {
+    'api_key': '4c934e67-5e04-4d19-953e-eac352d72f50',
+    'country_code': 'US'
+    
+}
+airlines = {
+    'iata_codes': [],
+    'icao_codes': []
+}
+#5 most common airlines used in US
+names = ["American Airlines", "Southwest Airlines", "Spirit Airlines", "Delta Air Lines", "United Airlines"]
+airline_response = requests.get('https://airlabs.co/api/v9/airlines', params=airline_params)
+if airline_response.status_code == 200:
+     airline_data = airline_response.json()
+     airline_data = airline_data.get('response', {})
+     for data in airline_data:
+         if data['iata_code'] != None and data['icao_code'] != None and data['name'] in names:
+            print(data['name'])
+            airlines['iata_codes'].append(data['iata_code'])
+            airlines['icao_codes'].append(data['icao_code'])
+
+
+for i in range(len(names)):
+    route_params = {
+        'api_key': '4c934e67-5e04-4d19-953e-eac352d72f50',
+        'dep_iata': origin_airport.get('iata_code'),
+        'dep_icao': origin_airport.get('icao_code'),
+        'arr_iata': des_airport.get("iata_code"),
+        'arr_icao': des_airport.get("icao_code"),
+        'airline_icao': airlines['icao_codes'][i],
+        'airline_iata': airlines['iata_codes'][i]
+    }
+
