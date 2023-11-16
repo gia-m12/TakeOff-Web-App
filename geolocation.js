ee912d7850cea15229c1cf09a6797fc317e8fb9b
Dillon Shi <dyoungshi004@gmail.com>
2023-11-16 18:26:58 -0500

    Sending user's coordinates to the back-end

--- geolocation.js
+++ geolocation.js
@@ -1,15 +1,26 @@
 // Check if the Geolocation API is available in the browser
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var lat = position.coords.latitude;
-        var lng = position.coords.longitude;
-
-        console.log("Your current location is:\nLatituted: " + lat 
-            + "\nLongitude: " + lng);
-    }, function(error) {
-        console.log("Error getting location: " + error.message);
-    });
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(sendPosition);
 } else {
     // Geolocation is not available in this browser
     console.log("Geolocation is not supported in your browser.");
+}
+
+function sendPosition(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+
+    // Send this data to your Python backend
+    fetch('http://localhost:5000/get_coordinates', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ lat, lng }),
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 }
